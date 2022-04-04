ae9009d6983cc52810ff9f55dcf37dea3d8683db
Dmitriy Oliferov <DrEfrit@yandex.ru>
2022-04-04 20:58:05 +0300

    add view_binding

--- app/src/main/java/com/oliferov/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/oliferov/cryptoapp/presentation/CoinDetailActivity.kt
@@ -2,21 +2,24 @@ package com.oliferov.cryptoapp.presentation
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.oliferov.cryptoapp.R
+import com.oliferov.cryptoapp.databinding.ActivityCoinDetailBinding
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_coin_detail.*
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
+    private val binding by lazy {
+        ActivityCoinDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_detail)
+        setContentView(binding.root)
         if (!intent.hasExtra(EXTRA_FROM_SYMBOL)) {
             finish()
             return
@@ -24,16 +27,18 @@ class CoinDetailActivity : AppCompatActivity() {
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
 
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this, Observer {
-            tvPriceDetail.text = it.price.toString()
-            tvMinPriceDetail.text = it.lowDay.toString()
-            tvMaxPriceDetail.text = it.highDay.toString()
-            tvLastMarketDetail.text = it.lastMarket
-            tvLastUpdateDetail.text = it.lastUpdate
-            tvFromSymbol.text = it.fromSymbol
-            tvToSymbol.text = it.toSymbol
-            Picasso.get().load(it.imageUrl).into(imageViewLogo)
-        })
+        viewModel.getDetailInfo(fromSymbol).observe(this) {
+            with(binding) {
+                tvPriceDetail.text = it.price.toString()
+                tvMinPriceDetail.text = it.lowDay.toString()
+                tvMaxPriceDetail.text = it.highDay.toString()
+                tvLastMarketDetail.text = it.lastMarket
+                tvLastUpdateDetail.text = it.lastUpdate
+                tvFromSymbol.text = it.fromSymbol
+                tvToSymbol.text = it.toSymbol
+                Picasso.get().load(it.imageUrl).into(imageViewLogo)
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/oliferov/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/oliferov/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.oliferov.cryptoapp.R
+import com.oliferov.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.oliferov.cryptoapp.domain.CoinInfo
 import com.oliferov.cryptoapp.presentation.adapters.CoinInfoAdapter
-import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
+    private val binding by lazy {
+        ActivityCoinPriceListBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_price_list)
+        setContentView(binding.root)
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
@@ -27,7 +31,7 @@ class CoinPriceListActivity : AppCompatActivity() {
                 )
             }
         }
-        rvCoinPriceList.adapter = adapter
+        binding.rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this) {
             adapter.coinPriceInfoList = it
